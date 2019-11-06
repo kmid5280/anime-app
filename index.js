02f0d1376ffba005693df88f27cee1345325cddd
Kyle Middleton <kylemid321@gmail.com>
2019-11-05 17:45:37 -0700

    add anime values for green and blue boxes, add delays

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 anime({
     targets: 'div.box.red',
-    translateY: [
-        { value: 100, duration: 500 },
+    translateX: [
+        { value: 300, duration: 500 },
         { value: 0, duration: 800 }
     ],
-})
+    rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine'
+    }
+});
+
+anime({
+    targets: 'div.box.blue',
+    translateX: [
+        { value: 300, duration: 500, delay: 1000 },
+        { value: 0, duration: 800 }
+    ],
+    rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine',
+        delay: 1000,
+    }
+});
+
+anime({
+    targets: 'div.box.green',
+    translateX: [
+        { value: 300, duration: 500, delay: 2000 },
+        { value: 0, duration: 800 }
+    ],
+    rotate: {
+        value: '1turn',
+        easing: 'easeInOutSine',
+        delay: 2000,
+    }
+});
